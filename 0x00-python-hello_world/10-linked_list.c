0aff69bf428bb6a723aaaabe2dbe4b191f4614ae
martinopolot <maopolot@outlook.com>
2022-11-28 14:13:07 -0800

    linked list

---
+++ 0x00-python-hello_world/10-linked_list.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * print_listint - prints all elements of all a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_listint_t(const listint_t *h)
+{
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
+
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
+
+	return (n);
+}
+
+/**
+ * add_nodeint - adds a new node at the beginnign of a listint_t list
+ * @head: pointer to a pointer of the start of the list
+ * @n: integer to be included in node
+ * Return: address of the new element or NULL if it fails
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}

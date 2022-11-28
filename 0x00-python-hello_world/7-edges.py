07acbb3e97e8e65191f34b3ee91c9faf8b80d3c7
martinopolot <maopolot@outlook.com>
2022-11-28 11:53:17 -0800

    7-edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_3_word = word[:3]
+last_word_2 = word[-2:]
+middle_word = word[1:-1]
+print("First three letters: {}".format(first_3_word))
+print("Last two letters: {}".format(last_word_2))
+print("Middle word: {}".format(middle_word))

c281efb6f671e285fffacb84c526cb6b8b503f79
martinopolot <maopolot@outlook.com>
2022-11-29 11:56:29 -0800

    islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def islower(ch):
+    """Function to check lower case characters """
+    if ord(ch) >= 97 and ord(ch) <= 122:
+        return True
+    else:
+        return False

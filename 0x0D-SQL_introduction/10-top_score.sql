47a4e9ae2246753c6bd7a3bec02b520704198d50
martinopolot <maopolot@outlook.com>
2023-02-14 13:57:29 -0800

    list by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
--- list all records of the table second_table of the database hbtn_0c_0
-SELECT scope, name FROM second_table ORDER BY score DESC;
+-- lists all records of the table second_table
+SELECT score, name FROM second_table ORDER BY score DESC;

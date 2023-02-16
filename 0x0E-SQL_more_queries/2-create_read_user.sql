6b078c0350f4132d5b6d783d6ca48af1b30575ae
martinopolot <maopolot@outlook.com>
2023-02-15 22:33:59 -0800

    read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,7 @@
--- 2-create_read_user.sql
+-- creates the database hbtn_0d_2 and the user user_0d_2C
+-- create a db
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- create a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grant user read rights
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;

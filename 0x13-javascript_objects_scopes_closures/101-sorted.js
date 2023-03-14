e2c6d61b7679a54cd1ab364fd65c127034b4379e
martinopolot <maopolot@outlook.com>
2023-03-14 15:17:34 -0700

    sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const original = require('./101-data').list;
-console.log(original);
-const mappedList = original.map (function (er, index) {
-  return (er * index);
+
+
+const firstList = require('./101-data').list;
+console.log(firstList);
+const mappedList = firstList.map (function (e, index) {
+  return (e * index);
 });
 console.log(mappedList);
